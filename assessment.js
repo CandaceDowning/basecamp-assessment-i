c513e679f898a76f1616fe5044bc22a813955611
Candace <candacedowning@gmail.com>
2018-09-22 12:52:17 -0500

    first mini project

--- assessment.js
+++ assessment.js
@@ -1,6 +1,6 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-// CODE HERE
+// CODE HERE in the future
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
